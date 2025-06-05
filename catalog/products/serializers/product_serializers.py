671eca6107c781c0727f84512f91e13b53d4295e
Leonidig <leonid200955@gmail.com>
2025-06-05 21:16:42 +0300

    nice

--- catalog/products/serializers/product_serializers.py
+++ catalog/products/serializers/product_serializers.py
@@ -1,36 +1,65 @@
 from django.contrib.auth.models import User
-from drf_spectacular.utils import extend_schema_field, OpenApiTypes
 from rest_framework import serializers
+from drf_spectacular.utils import extend_schema_field, OpenApiTypes
+import json
 
 from ..models import Product, Category
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
+    category = serializers.PrimaryKeyRelatedField(read_only=True)
     discount_price = serializers.SerializerMethodField()
 
     class Meta:
         model = Product
-        fields = [
-            "id",
-            "name",
-            "description",
-            "stock",
-            "price",
-            "available",
-            "category",
-            "nomenclature",
-            "created_at",
-            "rating",
-            "discount",
-            "attributes",
-            "discount_price",
-        ]
+        fields = ['name',
+                'description',
+                'price',
+                'stock',
+                'available',
+                'category',
+                'nomenclature',
+                'created_at',
+                'rating',
+                'discount',
+                'attributes',
+                'discount_price'
+                ]
 
     @extend_schema_field(OpenApiTypes.FLOAT)
     def get_discount_price(self, obj):
         return obj.discount_price
-
-    def clean_price(self, value):
+    
+    def validate_price(self, value):
         if value < 0:
-            return serializers.ValidationError('Error')
+            raise serializers.ValidationError('The price should be higher than 0')
+        return value
+        
+    def validate_stock(self, value):
+        if value < 0:
+            raise serializers.ValidationError('The stock should be higher than 0')
+        return value
+    
+    def validate_description(self, value):
+        if isinstance(value,str):
+            return value
+        raise serializers.ValidationError('Description must be text')
+    
+    def validate_category(self, value):
+        print(isinstance(value, int))
+        if not (isinstance(value, int)) or not (isinstance(value, Category)):
+            raise serializers.ValidationError("Category must be int or Category instance")
+        return value
+
+    def validate_discount(self, value):
+        if value < 0:
+            raise serializers.ValidationError('The discount should be higher than 0')
+        return value
+
+    def validate_attributes(self, value):
+        if not value:
+            return value
+        try:
+            return json.loads(value)
+        except Exception:
+            raise serializers.ValidationError('Attributes must be a valid JSON')
--- catalog/products/tests/test_serializers.py
+++ catalog/products/tests/test_serializers.py
@@ -1,47 +1,49 @@
 import pytest
-from .fixtures import category
+
+
+import pytest_check as check
 from products.serializers.product_serializers import ProductSerializer
 
+from .fixtures import category, product_discount, product, order
 
 
 @pytest.mark.django_db
 def test_product_serializer_valid(category):
     data = {
-        'name': 'test_name',
-        'description': 'test_desc',
-        'stock': 1,
-        'price': 100,
-        'category': category.id,
-        'nomenclature': 'test_nomenculature',
-        'rating': 2,
-        'discount': 30,
-        'attributes': dict()
+        "name": "test_name",
+        "description": "test_description",
+        "stock":3,
+        "price":100,
+        "available": True,
+        "category": category,
+        "nomenclature":"test_nomenclature",
+        "rating":2,
+        "discount":10,
+        "attributes": {}
     }
-
+    
     serializer = ProductSerializer(data=data)
+    
     assert serializer.is_valid()
-    assert not serializer.errors
-
-
-
+    
 @pytest.mark.django_db
 def test_product_serializer_invalid(category):
     data = {
         "name": "*"*101,
-        "description": 1,
-        "stock":-3,
+        "description": {},
+        "stock": -3,
         "price":-100,
         "available": 2,
-        "category": "test_category123",
         "nomenclature":"*"*101,
         "rating":"*",
         "discount":-10,
-        "attributes": []
+        "attributes": "*"
     }
     
     
-    
+
     serializer = ProductSerializer(data=data)
+    
     
     assert not serializer.is_valid()
     assert serializer.errors
@@ -51,4 +53,4 @@ def test_product_serializer_invalid(category):
     assert 'A valid number is required.' in serializer.errors["rating"]
     for field in data.keys():
         assert field in serializer.errors
-    print(dict(serializer.errors))
+    
